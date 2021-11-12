1cdee088347a84cf2c31a6b6d1bf38a91ee56df7
AUNG PAING <wizkhaap@gmail.com>
2021-11-12 16:38:55 +0630

    first

---
+++ lib/home.dart
@@ -0,0 +1,66 @@
+
+import 'dart:async';
+import 'dart:io';
+
+
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: WebView(
+        initialUrl: 'https://hd.yalla--live.com',
+        javascriptMode: JavascriptMode.unrestricted,
+        // onWebViewCreated: (WebViewController webViewController) {
+        //   _controller.complete(webViewController);
+        // },
+      ),
+    );
+  }
+}
+
+
+// class Home extends StatefulWidget {
+//   const Home({Key? key}) : super(key: key);
+//
+//
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
+//
+// class _HomeState extends State<Home> {
+//
+//  final Completer <WebViewController> _controller =Completer <WebViewController>();
+//
+// }
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body:WebView(
+//         initialUrl: 'https://hd.yalla--live.com',
+//         javascriptMode: JavascriptMode.unrestricted,
+//       )
+//     );
+//   }
+
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+
+class Myapp extends StatefulWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+
+
+  @override
+  _MyappState createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes:{
+        "/":(context)=>Home()
+      } ,
+
+    );
+  }
+}
+
